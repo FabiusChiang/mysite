d091833da68b5ba1b19e2e3fbf08a79eb9409c7d
fjiang4 <fabius.jiang@morningstar.com>
2017-10-16 22:50:56 -0500

    2 questions are done

---
+++ northwestern/10142017/homework.R
@@ -0,0 +1,35 @@
+##Exercise 4.59 ----------------------------------------------------
+
+density0To2 <- function(x) {
+    p <- 0.5
+    if (x <0 || x >2) {
+        p<- 0
+    }
+    p
+}
+
+drawShadeInEquallyLikely <- function(begin, end, title) {
+    seqX <- seq(begin,end,0.001)
+    cord.x <- c(begin,seqX,end) 
+    seqY <- rep(0.5, length(seqX))
+    cord.y <- c(0,seqY,0) 
+    h<-Vectorize(density0To2)
+    curve(h,xlim=c(-2,4),main=title,n=10000) 
+    polygon(cord.x,cord.y,col='skyblue')
+    abline(0,0)
+}
+
+## (a)
+drawShadeInEquallyLikely(0, 0, "Exercise 4.59 (a) Density curve 0~2")
+
+
+## (b)
+drawShadeInEquallyLikely(0, 1.6, "Exercise 4.59 (b) Density curve 0~2 and shade for 0-1.6")
+
+
+## (c)
+drawShadeInEquallyLikely(0.5, 1.7, "Exercise 4.59 (a) Density curve 0~2 and shade for 0.5-1.7")
+
+
+## (d)
+drawShadeInEquallyLikely(0.95, 2, "Exercise 4.59 (d) Density curve 0~2 and shade for 0.95-2")
