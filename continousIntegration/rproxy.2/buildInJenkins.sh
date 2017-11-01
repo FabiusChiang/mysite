34cbe7718cc7f62bcccb9656ea597b1b86491b85
fjiang4 <fabius.jiang@morningstar.com>
2017-11-01 15:19:16 -0500

    add rproxy.2 which supports reverse proxy and https

---
+++ continousIntegration/rproxy.2/buildInJenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BUILD_NUMBER=$1
+
+############Make enter the root folder of all script
+folderOfCurrentScript="$(dirname "${BASH_SOURCE}")"
+cd ${folderOfCurrentScript}
+
+appName=mysite
+componentName=rproxy
+version=`bash ../common/getVersion.sh`
+
+componentImageName=${appName}_${componentName}/${version}
+bash build.sh ${componentImageName} ${BUILD_NUMBER}
+bash ../common/tagAndPushImage.sh ${appName} ${BUILD_NUMBER} ${componentName}
+
+##Temporarilly, we don't need to create a package for deployment scripit.
+# bash ../common/createPackage.sh ${BUILD_NUMBER} ${componentName}
