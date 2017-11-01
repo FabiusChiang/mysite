fec2fb79cd2d96a16e7cac49b3daf8d6255015a1
fjiang4 <fabius.jiang@morningstar.com>
2017-11-01 15:20:09 -0500

    add newnode

--- newnode/index.js
+++ newnode/index.js
@@ -5,8 +5,8 @@ var request = require('request')
 // var request = Promise.promisifyAll(require("request"))
 const requestOptions = {
     // uri: "http://www.163.com",
-    uri: "https://www.google.com",
-    proxy: "http://localhost:8080"
+    uri: "https://www.google.com"
+    // ,proxy: "http://localhost:8080"
     // proxy: "http://192.168.31.13:10086"
 }
 
@@ -14,5 +14,15 @@ const requestOptions = {
 
 request.get(requestOptions.uri, requestOptions, function (error, rep) {
         console.log(rep.body);
+        if (rep.body){
+            console.log("body exists");
+        }
+        if (!rep.body2){
+            console.log("body2 not exists");
+        }
+        rep.body2 = "1bc";
+        if (rep.body2){
+            console.log(rep.body2);
+        }
     });
 
