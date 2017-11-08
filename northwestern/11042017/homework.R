ec8d1c0ee4a32dc482cfa96492744d7ce51f2a68
fjiang4 <fabius.jiang@morningstar.com>
2017-11-07 22:41:48 -0600

    6.137 is done

---
+++ northwestern/11042017/homework.R
@@ -0,0 +1,16 @@
+##Exercise 6.137 ----------------------------------------------------
+##Exercise 6.137----------------------------------------------------
+
+drawShadeInNorm <- function(begin, end, title, sigma) {
+    begin <- begin * sigma
+    end <- end * sigma
+    cord.x <- c(begin,seq(begin,end,0.01),end) 
+    cord.y <- c(0,dnorm(seq(begin,end,0.01), 0, sigma),0) 
+    curve(dnorm(x,0,sigma),xlim=c(-6 * sigma,6 * sigma),main=title) 
+    polygon(cord.x,cord.y,col='skyblue')
+    abline(0,0)
+}
+
+drawShadeInNorm(6.9/(55/sqrt(104)), 6, "Exercise 6.137 (a)", 55/sqrt(104))
+
+
