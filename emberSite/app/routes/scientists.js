ef618b120d7ebd47fe61e08485c0ae4670366e38
fjiang4 <fabius.jiang@morningstar.com>
2017-11-11 22:10:40 -0600

    MS-4: new ember site

---
+++ emberSite/app/routes/scientists.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+    model() {
+        return ["Fabius Jiang", "Lela Zhou Pig"];
+    }
+});
--- emberSite/dist/assets/tests.js
+++ emberSite/dist/assets/tests.js
@@ -1,42 +1,9 @@
 'use strict';
 
-define('ember-site/tests/acceptance/list-rentals-test', ['qunit', 'ember-site/tests/helpers/module-for-acceptance'], function (_qunit, _moduleForAcceptance) {
-  'use strict';
-
-  (0, _moduleForAcceptance.default)('Acceptance | list rentals');
-
-  (0, _qunit.test)('should show rentals as the home page', function (assert) {
-    visit('/');
-
-    andThen(function () {
-      assert.equal(currentURL(), '/rentals', 'should redirect automatically');
-    });
-  });
-
-  (0, _qunit.test)('Click link to contact information', function (assert) {
-    visit('/');
-    click('a:contains("About")');
-    andThen(function () {
-      assert.equal(currentURL(), '/about', 'should redirect automatically');
-    });
-  });
-
-  (0, _qunit.test)('The count of items in list should be 3', function (assert) {
-    visit('/');
-    andThen(function () {
-      assert.equal(find('.listing').length, 3, 'should see 3 listings');
-    });
-  });
-});
 define('ember-site/tests/app.lint-test', [], function () {
   'use strict';
 
   QUnit.module('ESLint | app');
-
-  QUnit.test('adapters/application.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'adapters/application.js should pass ESLint\n\n');
-  });
 
   QUnit.test('app.js', function (assert) {
     assert.expect(1);
@@ -53,24 +20,9 @@ define('ember-site/tests/app.lint-test', [], function () {
     assert.ok(true, 'router.js should pass ESLint\n\n');
   });
 
-  QUnit.test('routes/about.js', function (assert) {
+  QUnit.test('routes/scientists.js', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'routes/about.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('routes/contact.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'routes/contact.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('routes/index.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'routes/index.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('routes/rentals.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'routes/rentals.js should pass ESLint\n\n');
+    assert.ok(true, 'routes/scientists.js should pass ESLint\n\n');
   });
 });
 define('ember-site/tests/helpers/destroy-app', ['exports'], function (exports) {
@@ -80,11 +32,9 @@ define('ember-site/tests/helpers/destroy-app', ['exports'], function (exports) {
     value: true
   });
   exports.default = destroyApp;
+  var run = Ember.run;
   function destroyApp(application) {
-    Ember.run(application, 'destroy');
-    if (window.server) {
-      window.server.shutdown();
-    }
+    run(application, 'destroy');
   }
 });
 define('ember-site/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'ember-site/tests/helpers/start-app', 'ember-site/tests/helpers/destroy-app'], function (exports, _qunit, _startApp, _destroyApp) {
@@ -142,11 +92,13 @@ define('ember-site/tests/helpers/start-app', ['exports', 'ember-site/app', 'embe
     value: true
   });
   exports.default = startApp;
+  var merge = Ember.merge;
+  var run = Ember.run;
   function startApp(attrs) {
-    var attributes = Ember.merge({}, _environment.default.APP);
-    attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+    var attributes = merge({}, _environment.default.APP);
+    attributes = merge(attributes, attrs); // use defaults, but you can override;
 
-    return Ember.run(function () {
+    return run(function () {
       var application = _app.default.create(attributes);
       application.setupForTesting();
       application.injectTestHelpers();
@@ -164,11 +116,6 @@ define('ember-site/tests/tests.lint-test', [], function () {
   'use strict';
 
   QUnit.module('ESLint | tests');
-
-  QUnit.test('acceptance/list-rentals-test.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'acceptance/list-rentals-test.js should pass ESLint\n\n');
-  });
 
   QUnit.test('helpers/destroy-app.js', function (assert) {
     assert.expect(1);
@@ -195,88 +142,15 @@ define('ember-site/tests/tests.lint-test', [], function () {
     assert.ok(true, 'test-helper.js should pass ESLint\n\n');
   });
 
-  QUnit.test('unit/adapters/application-test.js', function (assert) {
+  QUnit.test('unit/routes/scientists-test.js', function (assert) {
     assert.expect(1);
-    assert.ok(true, 'unit/adapters/application-test.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('unit/routes/about-test.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'unit/routes/about-test.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('unit/routes/contact-test.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'unit/routes/contact-test.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('unit/routes/index-test.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'unit/routes/index-test.js should pass ESLint\n\n');
-  });
-
-  QUnit.test('unit/routes/rentals-test.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'unit/routes/rentals-test.js should pass ESLint\n\n');
+    assert.ok(true, 'unit/routes/scientists-test.js should pass ESLint\n\n');
   });
 });
-define('ember-site/tests/unit/adapters/application-test', ['ember-qunit'], function (_emberQunit) {
+define('ember-site/tests/unit/routes/scientists-test', ['ember-qunit'], function (_emberQunit) {
   'use strict';
 
-  (0, _emberQunit.moduleFor)('adapter:application', 'Unit | Adapter | application', {
-    // Specify the other units that are required for this test.
-    // needs: ['serializer:foo']
-  });
-
-  // Replace this with your real tests.
-  (0, _emberQunit.test)('it exists', function (assert) {
-    var adapter = this.subject();
-    assert.ok(adapter);
-  });
-});
-define('ember-site/tests/unit/routes/about-test', ['ember-qunit'], function (_emberQunit) {
-  'use strict';
-
-  (0, _emberQunit.moduleFor)('route:about', 'Unit | Route | about', {
-    // Specify the other units that are required for this test.
-    // needs: ['controller:foo']
-  });
-
-  (0, _emberQunit.test)('it exists', function (assert) {
-    var route = this.subject();
-    assert.ok(route);
-  });
-});
-define('ember-site/tests/unit/routes/contact-test', ['ember-qunit'], function (_emberQunit) {
-  'use strict';
-
-  (0, _emberQunit.moduleFor)('route:contact', 'Unit | Route | contact', {
-    // Specify the other units that are required for this test.
-    // needs: ['controller:foo']
-  });
-
-  (0, _emberQunit.test)('it exists', function (assert) {
-    var route = this.subject();
-    assert.ok(route);
-  });
-});
-define('ember-site/tests/unit/routes/index-test', ['ember-qunit'], function (_emberQunit) {
-  'use strict';
-
-  (0, _emberQunit.moduleFor)('route:index', 'Unit | Route | index', {
-    // Specify the other units that are required for this test.
-    // needs: ['controller:foo']
-  });
-
-  (0, _emberQunit.test)('it exists', function (assert) {
-    var route = this.subject();
-    assert.ok(route);
-  });
-});
-define('ember-site/tests/unit/routes/rentals-test', ['ember-qunit'], function (_emberQunit) {
-  'use strict';
-
-  (0, _emberQunit.moduleFor)('route:rentals', 'Unit | Route | rentals', {
+  (0, _emberQunit.moduleFor)('route:scientists', 'Unit | Route | scientists', {
     // Specify the other units that are required for this test.
     // needs: ['controller:foo']
   });
