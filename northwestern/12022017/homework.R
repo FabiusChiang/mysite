3c63d37354798bced30b48d745c7006611d0586f
fjiang4 <fabius.jiang@morningstar.com>
2017-12-03 23:21:58 -0600

    10.46 (e) is not done

--- northwestern/12022017/homework.R
+++ northwestern/12022017/homework.R
@@ -51,3 +51,29 @@ summary(modelFilteredAS)
 dataForScatterplot<-data.frame(filteredData$AssessedValue, filteredData$SalesPrice)
 plot(dataForScatterplot, xlab="Assessed Value", ylab="Sales Price", main="Exercise 10.46 (a)")
 abline(modelFilteredAS, col='blue')
+
+## (b)
+estimateY <- function(x){
+    9.43181 + 1.123 * x
+}
+estimatedSalesPrice<-sapply(filteredData$AssessedValue, estimateY, simplify="array")
+
+
+diffOfY<-filteredData$SalesPrice-estimatedSalesPrice
+
+hist(diffOfY, bre=6, main="10.46 (b) The difference between observed Y and the estimated Y")
+## The value of residuals don't match Normal but "linear + Normal noise".
+## From the histograph of the difference between observed Y and the estimated Y, we can infer the "difference" matches Normal roughly
+
+## (c)
+## Yes, the statistical inference is satified -- the difference between observed Y and the estimated Y is due to the random noise, which matches Normal roughly.
+
+## (d)
+#From the summary(modelFilteredAS) we know the mean of slope is 1.123 and the standard error of slope is 0.08295
+#The corresponding t of the 95% confidence level is:
+t<-qt(0.95+(1-0.95)/2, df=32)
+m<-t*0.08295
+1.123-m
+1.123+m
+
+## (e)
