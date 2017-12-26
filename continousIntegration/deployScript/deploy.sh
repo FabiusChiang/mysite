5b02941b486906cf9fa80ffff1fd035cc5e1abee
fjiang4 <fabius.jiang@morningstar.com>
2017-12-25 20:18:50 -0600

    for deployment in new server

--- continousIntegration/deployScript/deploy.sh
+++ continousIntegration/deployScript/deploy.sh
@@ -17,19 +17,19 @@ baseImagesurl="fabius/"${appName}":"
 
 #################################################################
 ##MySql
-# mysqlPassword="1234567"
-# docker stop mysql
-# docker rm mysql
-# docker run --name mysql -e MYSQL_ROOT_PASSWORD=${mysqlPassword} -v /home/fabius/data/mysql:/var/lib/mysql -d mysql:5.7
+mysqlPassword="1234567"
+docker stop mysql
+docker rm mysql
+docker run --name mysql -e MYSQL_ROOT_PASSWORD=${mysqlPassword} -v /home/fabius/data/mysql:/var/lib/mysql -d mysql:5.7
 
 
 #################################################################
 ##WordPress
-# port=`bash ../azureCommon/allocatePort.sh wordpress ${appEnv}`
-# wordPressContainerName=${appName}_wordpress_${appEnv}
-# docker stop ${wordPressContainerName}
-# docker rm ${wordPressContainerName}
-# docker run -p ${port}:80 --name ${wordPressContainerName} --link mysql:mysql -e WORDPRESS_DB_HOST=mysql -e WORDPRESS_DB_PASSWORD=${mysqlPassword} -e WORDPRESS_DB_NAME=wordpress_${appEnv} -d wordpress:4.8.3-apache
+port=`bash ../azureCommon/allocatePort.sh wordpress ${appEnv}`
+wordPressContainerName=${appName}_wordpress_${appEnv}
+docker stop ${wordPressContainerName}
+docker rm ${wordPressContainerName}
+docker run -p ${port}:80 --name ${wordPressContainerName} --link mysql:mysql -e WORDPRESS_DB_HOST=mysql -e WORDPRESS_DB_PASSWORD=${mysqlPassword} -e WORDPRESS_DB_NAME=wordpress_${appEnv} -d wordpress:4.8.3-apache
 
 
 #################################################################
