44a32453d25afa57d54fcf655a694f3e01c4ddca
Fabius Chiang <fabiushust@hotmail.com>
2018-02-26 08:34:09 -0600

    MS-14: group hw 1

--- northwestern/calculcateErlangB/calculcateErlangB/g1.js
+++ northwestern/calculcateErlangB/calculcateErlangB/g1.js
@@ -2,11 +2,41 @@
 
 const calc = require("./funcs");
 
-const bht = calc.ErlangBBHT(0.02, 60);
-const lines = calc.ErlangBLines(49.6, 0.02);
-const blockProbability = calc.ErlangB(49.6, 60);
+const sourceData = [];
+
+let pb = 0.01;
+for (let i = 0; i < 10; i++) {
+    sourceData.push({
+        pb: pb + pb * i,
+        line: 24
+    });
+}
+
+function fillData(dataArray) {
+    dataArray.forEach(item => {
+        if (!item.bht) {
+            item.bht = calc.ErlangBBHT(item.pb, item.line);
+        }
+        else if (!item.pb) {
+            item.pb = calc.ErlangBBHT(item.pb, item.line);
+        }
+        else if (!item.line) {
+            item.line = calc.ErlangBBHT(item.pb, item.line);
+        }
+    });
+}
 
 
-console.log(bht);
-console.log(lines);
-console.log(blockProbability);
+function getUsersInBatch(dataArray) {
+    dataArray.forEach(item => {
+        item.bht = calc.ErlangBBHT(item.pb, 60);
+        item.users = item.bht / 0.1;
+        console.log(item);
+    });
+}
+
+getUsersInBatch(sourceData);
+
+//console.log(bht);
+//console.log(lines);
+//console.log(blockProbability);
