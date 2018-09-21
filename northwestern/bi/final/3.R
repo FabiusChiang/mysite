92bd638d410a0f121c7ebb4af4dc6008cd47ae67
Fabius Chiang <fabiushust@hotmail.com>
2018-09-21 13:40:16 -0500

    bi homework

---
+++ northwestern/bi/final/3.R
@@ -0,0 +1,14 @@
+head(iris)
+plot(iris[, 1:4], col=iris$Species, pch=16)
+
+summary(iris$Sepal.Length)
+sd(iris$Sepal.Length)
+
+summary(iris$Sepal.Width)
+sd(iris$Sepal.Width)
+
+summary(iris$Petal.Length)
+sd(iris$Sepal.Length)
+
+summary(iris$Petal.Width)
+sd(iris$Sepal.Width)
