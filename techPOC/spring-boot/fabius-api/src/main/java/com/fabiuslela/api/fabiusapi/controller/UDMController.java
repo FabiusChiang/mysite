3bba924b47da76090c693c4e946be873875dac1f
Fabius <Fabius.Jiang@ms.com>
2019-05-13 22:08:36 -0500

    MS-34: setup spring-boot application

---
+++ techPOC/spring-boot/fabius-api/src/main/java/com/fabiuslela/api/fabiusapi/controller/UDMController.java
@@ -0,0 +1,43 @@
+package com.fabiuslela.api.fabiusapi.controller;
+
+// import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+// import javax.servlet.http.HttpServletRequest;
+// import java.util.List;
+
+/**
+ * Created by Fabius Jiang
+ * <p>
+ * UDM controller provides basic information of the service to serve monitor requests.
+ */
+@RestController
+@RequestMapping("/udm")
+public class UDMController {
+
+    // @Autowired
+    // PublishService publishService;
+
+    /**
+     * Publishes an advisor proposal.  If a different document type is to be introduced,
+     * a separate Controller and endpoint should be added along with an applicable ReportType
+     * in pub-common
+     *
+     * @return
+     */
+    // @CrossOrigin
+    // @ResponseBody
+    // @RequestMapping(path = "/proposal/publish_bak", method = RequestMethod.POST)
+    // public EndpointResponse publishProposal(@RequestBody ApiProposalRequest proposalRequest, HttpServletRequest request) {
+    //     RequestContext context = createRequestContext(request);
+    //     String submissionId = publishService.generateSubmissionId();
+    //     List<SubmissionResult> results = publishService.queueRequests(submissionId, proposalRequest, context);
+    //     return ControllerResponseUtility.convertToStatusResponse(submissionId, results);
+    // }
+
+    @GetMapping("/{id}")
+    public String findOne(@PathVariable String id) {
+        return id + " is received!";
+    }
+
+}
