bcfaf1901b3a38928f4d6d810d379222031f32f4
Fabius <Fabius.Jiang@ms.com>
2019-07-08 21:56:29 -0500

    MS-4: the first step to test test vuex

--- vueSite/src/components/FabiusIntro.vue
+++ vueSite/src/components/FabiusIntro.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="col-sm-8">
+      
         <h2>Fabius introduction</h2>
         <h5>Title1 description, Dec 7, 2017</h5>
         <div class="fakeimg">Fake Image</div>
         <div class="pageBody">
           <managedContent v-bind:dataSource="url" />
+          <h6 class="testVue">{{count0}}</h6>
         </div>
     </div>
 </template>
 
 <script>
 import managedContent from './ManagedContent.vue';
+import { mapState } from 'vuex';
 
 export default {
     name: 'Fabius',
@@ -19,7 +22,12 @@ export default {
       return {
         url: "http://blog.fabiuslela.com/fabiuss-introduction/"
       }
-    }
+    },
+    computed: mapState({
+        count0(store) {
+          return store.count0;
+        }
+      })
 }
 </script>
 
@@ -40,5 +48,9 @@ a {
 }
 .pageBody{
   text-align: left;
+}
+.testVue{
+  color: lightblue;
+  font: 5em sans-serif;
 }
 </style>
--- vueSite/src/main.js
+++ vueSite/src/main.js
@@ -3,13 +3,28 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+
 
 Vue.config.productionTip = false
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count0: 0,
+    count1: 1
+  }
+})
+
+
+
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
